74fb67d72e6a2b2a41e04c151da60552e826bf4f
JosephWinchester <65713087+JosephWinchester@users.noreply.github.com>
2023-04-17 10:42:08 +0300

    Update 7.cpp

--- Новая папка/6/7.cpp
+++ Новая папка/6/7.cpp
@@ -1,41 +1,58 @@
-#include <iostream>   
-using namespace std;
-class Angle
-{
- private:
-  int degree;
-  float min;
-  char dir;
- public:
-  Angle() : degree(0), min(0), dir(0)
-  {}
-  Angle(int deg, float m, char d) : degree(deg), min(m), dir(d)
-  {}
-  void get_dir()
-  {
-   cout << "Enter degree" << endl; cin >> degree;
-   cout << "Enter minutes" << endl; cin >> min;
-   cout << "Enter direction (N, S, E, W)" << endl; cin >> dir;
-  }
-  void show_dir()
-  {
-   cout << degree << '\xF8' << min << "'" << dir << endl;
-  }
+#include<iostream>
 
+using namespace std;
+
+class angle{
+    int degrees;
+    float minutes;
+    char direction;
+    public:
+        angle():degrees(0), minutes(0), direction(0) {	}
+		angle(int deg, float min, char dir):degrees(deg), minutes(min), direction(dir) {	}
+
+        void getData(int, float, char);
+        void display() const;
 };
-int main()
-{
- Angle angl_1(179, 59.9, 'E');
- Angle angl_2;
- char ch;
- angl_1.show_dir();
- do
- {
-  angl_2.get_dir();
-  angl_2.show_dir();
-  cout << "repeat? (y/n)" << endl;
-  cin >> ch;
- }
- while (ch != 'n');
- return 0;
+
+void angle::getData(int dgr, float mnt, char dir){
+    degrees = dgr;
+    minutes = mnt;
+    direction = dir;
+}
+
+void angle::display() const{
+    cout << "Angle: " << degrees << "\xF8 " << minutes << "\' " << direction;
+    if(direction == 'E' || direction == 'e' || direction == 'W' || direction == 'w') {
+		cout << " Longitude !";
+	} if(direction == 'N' || direction == 'n' || direction == 'S' || direction == 's') {
+		cout << " Latitude !";
+	}
+}
+
+int main(){
+    int degr;
+	float mint;
+	char drct, choice;
+	cout << endl;
+	angle angle1(146, 34.6, 'S');
+	angle1.display();	
+	cout << endl;
+	angle angle2;
+
+	do {
+		cout << "\nEnter the degree: ";
+		cin >> degr;
+		cout << "Enter the minutes: ";
+		cin >> mint;
+		cout << "Enter the direction(N, S, E, W): ";
+		cin >> drct;
+
+		angle2.getData(degr, mint, drct);
+		angle2.display();
+
+		cout << "\nDo you want to enter another angle for navigation (Y, N): ";
+		cin >> choice;
+	} while(choice == 'Y' || choice == 'y' );
+	cout << endl;
+    return 0;
 }
