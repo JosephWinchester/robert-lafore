579eb39e6538e0e3a834ed3564aedd01e4f8fbfd
JosephWinchester <65713087+JosephWinchester@users.noreply.github.com>
2023-04-17 10:41:30 +0300

    Update 6.cpp

--- Новая папка/6/6.cpp
+++ Новая папка/6/6.cpp
@@ -1,74 +1,101 @@
-#include <iostream>   
+#include<iostream>
+
 using namespace std;
-class Date
-{
- private:
- public:
-  int day, month, year;
-  Date() : month(0), day(0), year(0)
-  {}
-  Date(int d, int m, int y) : month(m), day(d), year(y)
-  {}
+
+enum etype {laborer, secretary, manager, accountant, executive, researcher};	
+
+class dates {	
+	private:
+		int day, month, year;
+	public:
+		void getDate(){
+            char temp;
+	        cout << "Enter employee date of joining in (DD/MM/YYYY) format: ";
+	        cin >> day >> temp >> month >> temp >>year;
+        }	
+        void showDate(){
+            cout << "\nEmployee date of joining: " << day << "/" << month << "/" <<year;
+        }
 };
-class Employee
-{
- private:
-  int number;
-  float salary;
-  Date date;
-  enum etype {laborer, secretary, manager, accountant, executive, researcher};
-  etype post;
-  char letter;
- public:
-  Employee() : number(0), salary(0)
-  {}
-  Employee(int num, float sal) : number(num), salary(sal)
-  {}
-  void get_empl()
-  {
-   char ch;
-   cout << "number of the employee" << endl;
-   cin >> number;
-   cout << "salary" << endl;
-   cin >> salary;
-   cout << "enter day, month and year of employment (XX/XX/XXXX) " << endl;
-   cin >> date.day >> ch >> date.month >> ch >> date.year;
-   cout << "enter first letter of post (laborer, secretary, manager, accountant, executive, researcher)" << endl;
-   cin >> letter;
-   switch (letter)
-   {
-    case 'l': post = laborer; break;
-    case 's': post = secretary; break;
-    case 'm': post = manager; break;
-    case 'a': post = accountant; break;
-    case 'e': post = executive; break;
-    case 'r': post = researcher; break;
-    }
- 
-  }
-  void put_empl()const
-  {
-   cout << "employee " << number << ", salary " << salary << "$," << " date of employment " << date.day << "/" << date.month << "/" << date.year << ", post ";
-    switch(post)
-    {
-    case laborer: cout << "laborer" << endl; break;
-    case secretary: cout << "secretary" << endl; break;
-    case manager: cout << "manager" << endl; break;
-    case accountant: cout << "accountant" << endl; break;
-    case executive: cout << "executive" << endl; break;
-    case researcher: cout << "researcher" << endl; break;
-    }
-  };
+
+class employee {
+	private:
+		int emp_num;
+        float emp_comp;
+        dates date1;
+        etype emp;
+	public:
+		void setData();
+        void dispData();
 };
-int main()
-{
- Employee empl_1, empl_2, empl_3;
- empl_1.get_empl();
- empl_2.get_empl();
- empl_3.get_empl();
- empl_1.put_empl();
- empl_2.put_empl();
- empl_3.put_empl();
- system ("pause");
- return 0;
+
+void employee::setData(){
+    char type;
+    cout << "\nEnter employee Number: ";
+	cin >> emp_num;
+	cout << "Enter employee Compensation: ";
+	cin >> emp_comp;
+    date1.getDate();
+    cout << "Enter employee type (first letter only laborer, secretary, manager, accountant, executive, researcher): ";
+    cin >> type;
+    switch (type){
+        case 'l': emp = laborer; break;
+        case 's': emp = secretary; break;
+        case 'm': emp = manager; break;
+        case 'a': emp = accountant; break;
+        case 'e': emp = executive; break;
+        case 'r': emp = researcher; break;
+        default:
+            break;
+    }
+}
+
+void employee::dispData(){
+    cout << "Employee Number: " << emp_num << endl;
+	cout << "Employee Compensation: $" << emp_comp <<endl;
+    date1.showDate();
+    cout << endl;
+    switch(emp){
+        case laborer:
+            cout << "Employee type is laborer";
+            break;
+        case secretary:
+            cout << "Employee type is secretary";
+            break;
+        case manager:
+            cout << "Employee type is manager";
+            break;
+        case accountant:
+            cout << "Employee type is accountant";
+            break;
+        case executive:
+            cout << "Employee type is executive";
+            break;
+        case researcher:
+            cout << "Employee type is researcher";
+            break;
+        default:
+            cout << "Invalid input";
+            break;
+    }
+    cout << endl;
+}
+
+int main(){
+    employee emp1, emp2, emp3;
+	cout <<"\nEnter first employee details: \n";
+	emp1.setData();	
+	cout <<"\nEnter second employee details: \n";
+	emp2.setData();	
+	cout <<"\nEnter third employee details: \n";
+	emp3.setData();	
+
+	cout <<"\nFirst employee deatils: ";
+	emp1.dispData();
+	cout <<"\nSecond employee deatils: ";
+	emp2.dispData();
+	cout <<"\nThird employee deatils: ";
+	emp3.dispData();
+    
+	return 0;
 }
