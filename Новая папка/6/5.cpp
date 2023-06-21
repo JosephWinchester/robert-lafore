2c777e169abbefa93cebd823456be6280bac73e4
JosephWinchester <65713087+JosephWinchester@users.noreply.github.com>
2023-06-21 10:12:43 +0300

    Update 5.cpp

--- Новая папка/6/5.cpp
+++ Новая папка/6/5.cpp
@@ -1,25 +1,41 @@
-#include<iostream>
-
+#include <io.h> 
+#include <fcntl.h> 
+#include <iostream>
 using namespace std;
 
-class date{
-    private:
-        int month, day, year;
-        char temp;
-    public:
-        date(){ };
-        void getData(){
-            cout << "Enter date (MM/DD/YYYY): ";
-            cin >> month >> temp >> day >> temp >> year;
-        }
-        void display(){
-            cout << endl << "Date is " << month << "/" << day << "/" << year << endl;
-        }
+class date 
+{
+private:
+	int day;  
+	int month; 
+	int year; 
+public:
+
+	void get()
+	{
+		wchar_t slash; 
+		wcout << L"Введите день, месяц и год через косую черту (/): ";
+		wcin >> day >> slash >> month >> slash >> year;
+	}
+	
+	void display() const
+		{ wcout << day << L"/" << month << L"/" << year; }
 };
 
-int main(){
-    date d1;
-    d1.getData();
-    d1.display();
-    return 0;
+int main()
+{
+
+	_setmode(_fileno(stdout), _O_U16TEXT);
+
+	_setmode(_fileno(stdin), _O_U16TEXT);
+	
+	date d; 
+
+
+	d.get();
+
+	
+	wcout << L"Вы ввели дату: "; d.display(); wcout << endl;
+
+	return 0;
 }
